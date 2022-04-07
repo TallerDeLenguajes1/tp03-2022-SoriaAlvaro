174889f2e3291ed02c670d745acc1a1566b7bbfb
Soria Alvaro <alvarosoria.universidad@gmail.com>
2022-04-07 19:18:22 -0300

    casi terminado

--- tp3_2.c
+++ tp3_2.c
@@ -3,25 +3,25 @@
 #include <stdlib.h>
 
 int main (){
-    //char *nombres=(char *) malloc(5 * sizeof(char));  -> reseva 5 caracteres
-    //char *nombres=(char *) malloc((strlen(aux)+1) * sizeof(char)); -> reserva la cantidad de caracteres que se ingresen en aux
-    char *nombre[]=(char *) malloc(5 * sizeof(char));
-    char *aux=(char*)malloc(100 * sizeof(char));
-    *nombre=(char *) malloc((strlen(aux)+1) * sizeof(char));
+    /* char *nombres=(char *) malloc(5 * sizeof(char));  -> reseva 5 caracteres */
+    /* char *nombres=(char *) malloc((strlen(aux)+1) * sizeof(char)); -> reserva la cantidad de caracteres que se ingresen en aux */
+    char *nombre[5];
+    char *aux;
     int i,j;
     for(i=0;i<5;i++){
+        aux=(char*)malloc(100 * sizeof(char));
         printf("Ingrese el nombre...\n");
         gets(aux);
+        nombre[i]=(char *) malloc((strlen(aux)+1) * sizeof(char));
         strcpy(nombre[i],aux);
     }
+    free(aux);
+
     for(j=0;j<5;j++){
         printf("\n\nNombre.....\n");
-        puts(nombre[i]);
+        puts(nombre[j]);
     }
-    
 
-
-    free(aux);
     free(nombre);
 
     return 0;
